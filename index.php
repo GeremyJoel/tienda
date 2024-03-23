2b4a388d805791faeba6f7a3d359872af1e8a878
GeremyJoel <geremyjoel97@gmail.com>
2024-03-22 21:13:56 -0600

    Mi primer commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <header>
+        <h2>Bienvenido Geremy</h2>
+        <ul>
+            <li><a href="#"> Inicio</a></li>
+            <li><a href="#"> Inicio Sesión</a></li>
+            <li><a href="#"> Registro</a></li>
+        </ul>
+    </header>
+    <section>
+        
+    </section>
+</body>
+</html>
